2abc8875109aa1cfe6a364c13693ab43a61bb536
ujala shanker <ujalashanker@gmail.com>
2015-02-10 17:07:53 -0800

    StockPicker
    pair programming with ssd

---
+++ StockPicker.rb
@@ -0,0 +1,31 @@
+def stockpicker(array)
+	diff= []
+	collector=[]
+	hash= Hash.new
+
+	#stockpicker
+	if array[0]==array.max
+	     array.delete(array.max)
+	end
+
+
+	until array.length < 1
+	     for i in 0...array.index(array.max)
+	          collector.push(array[i])
+	     end
+
+	     d=array.max.to_i-collector.min.to_i
+
+	     hash["#{collector.min.to_s} , #{array.max.to_s}"] = d
+	     diff.push(d)
+	     collector=[]
+	     array.delete(array.max)
+
+
+	end
+
+	a=hash.max_by{|k,v| v}
+	p a[0]
+
+end
+stockpicker([17,3,6,9,15,8,6,1,10])
